2b510a66b0ebc6e7855bbf36f1817a5f6d6474a6
CollinOS <89503725+CollinOS@users.noreply.github.com>
2022-11-15 13:02:48 -0500

    fixed build error

--- pages/account/index.jsx
+++ pages/account/index.jsx
@@ -1,6 +1,6 @@
 import { Auth, ThemeSupa } from '@supabase/auth-ui-react'
 import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
-import Account from '../../components/accounts'
+import Account from '../../components/Accounts'
 
 const Index = () => {
   const session = useSession()
