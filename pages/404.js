79aeafef5e9fb8b66b355c099e4c1812f3f6f6c5
Collin Osborne <collinos@proton.me>
2022-12-22 15:06:29 -0500

    adding 404 page

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <main className='page-center text-center'>
+        <h1 className='text-purple'>404</h1>
+        <h2>Page Not Found</h2>
+    </main>
+  )
+}
+
+export default Error
